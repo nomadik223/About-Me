87da1dd87c0f7ca3b7c1a0af6d678e9c71066126
Austin Rogers <contactALRogers@gmail.com>
2017-01-06 11:05:04 -0800

    Added CSS and app.js fixes

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 //Helps make sure that there is stricter debugging in the code.
 'use strict';
 
-//Variable to keep tally of correct answers.
-var correct = 0;
-
 //Confirmation status
 var EntryAnswer = confirm('ARE YOU READY?');
 var correct = 0;
 
 if (EntryAnswer) {
   alert('Player 1 has joined the server. Stand by for Match Start.');
+  document.write('<p>Results for Entry Quiz. User chose ready. Game started.</p>');
 } else {
   alert('Too bad. Connecting to server.');
+  document.write('<p>Results for Entry Quiz. User chose not ready. Began Anyway.</p>');
 }
 
 //Name entry
 var contestantName = prompt('What is your name?');
 
-alert('Welcome ' + contestantName + ' to a little Quiz about me, Austin. Please hit enter to continue.');
+if (contestantName === null){
+  contestantName = '(NO NAME ENTERED)';
+}
 
+alert('Welcome ' + contestantName + ' to a little Quiz about me, Austin. Please hit enter to continue.');
+document.write('<p> User ' + contestantName + ' began the quiz.</p>');
 //Q1
 
 var answer1 = prompt('How old do you think I am? Please answer with digits, not a typed out answer.');
@@ -33,9 +36,10 @@ function functionAnswer1(answer1) {
 
 if (functionAnswer1(answer1)) {
   console.log('Correct! I am indeed only 19 years old!');
-
+  document.write('<p> Am I 19? ' + contestantName + ' guessed correctly! I am 19.</p>');
 } else {
   console.log('Question 1 was Incorrect');
+  document.write('<p> Am I 19?' + contestantName + ' guessed incorrectly. I am only 19.</p>');
   alert('Ooh, sorry. That was wrong. I am actually 19.');
 }
 
@@ -52,10 +56,11 @@ function functionAnswer2(answer){
 
 if (functionAnswer2(answer2)) {
   console.log('Correct! Jazz, Thumper, Fezzik, Arya and Maou!');
-
+  document.write('<p>Have I had 5 dogs throughout my life so far? ' + contestantName + ' guessed correctly! I have indeed. Their names are Jazz, Thumper, Fezzik, Arya and Maou.</p>');
 } else {
   alert('Oh, I\'m sorry! the correct answer was yes!');
   console.log('Question 2 was Incorrect');
+  document.write('<p>Have I had 5 dogs throughout my life so far? ' + contestantName + ' guessed incorrectly. I have indeed had 5 dogs throughout my life. Their names are Jazz, Thumper, Fezzik, Arya and Maou.</p>');
 }
 
 //Q3
@@ -71,10 +76,11 @@ function functionAnswer3(answer3) {
 
 if (functionAnswer3(answer3)) {
   console.log('Correct! Cats just don\'t seem to like my family.');
-
+  document.write('<p>Have I owned any cats? ' + contestantName + ' guessed correctly. Cats seem to hate me, so no.</p>');
 } else {
   alert('Oh, I\'m sorry! My family has not owned any cats.');
   console.log('Question 3 was Incorrect');
+  document.write('<p>Have I ever owned a cat? ' + contestantName + ' guessed that I have had a cat. They were wrong; cats hate me.</p>');
 }
 
 //Q4
@@ -90,10 +96,11 @@ function functionAnswer4(answer4) {
 
 if (functionAnswer4(answer4)) {
   console.log('Correct! A couple favorites being Payday 2 and the Pokemon games!');
-
+  document.write('<p> Do I play a lot of games from the FPS and PRG genres? ' + contestantName + ' guessed I do, and he is absolutely correct. I have literal thousands of hours in to my Pokemon games alone.</p>');
 } else {
   alert('Oh, I\'m sorry! I mostly play games from those Genres.');
   console.log('Question 4 was Incorrect');
+  document.write('<p>Do I play a lot of games from the FPs and RPG genres? ' + contestantName + ' guessed I do not, and they would be wrong. I Very much play games from those genres.</p>');
 }
 
 //Q5
@@ -109,13 +116,14 @@ function functionAnswer5(answer5) {
 
 if (functionAnswer5(answer5)) {
   console.log('Correct! I have only been writing fanfiction for the past week or so.');
-
+  document.write('<p>Have I been writing fanfiction for the past year? ' + contestantName + ' thought I did not, and he is correct. I have only very recently began writing fanfiction.</p>');
 } else {
   alert('Oh, I\'m sorry! While I do write Fanfiction, I only recently began, starting before the new year.');
   console.log('Question 5 was Incorrect');
+  document.write('<p>Have I been writing fanfiction for the past year? ' + contestantName + ' thinks I have, which is wrong. I have only been writing for a very short time.</p>');
 }
 
-for(var index = 0; index < 4; index++){
+/* for(var index = 0; index < 4; index++){
   var answer6 = parseInt(prompt('How many brothers do I have? Please answer with digits, not typed out numbers. Hint: It is less than 8'));
   if(answer6 === 2){
     alert('Correct! Ian and Logan!');
@@ -133,7 +141,7 @@ if(correctQuestion6 != true){
   alert('I am sorry, but I have 2 brothers!');
   console.log('Question 6 was incorrect');
 }
-
+*/
 //6
 
 //declare function
@@ -143,6 +151,7 @@ function functionAnswer6(answer6) {
     console.log('Question 6 was correct with ' + index + ' wrong guesses.');
     index = 7;
     correct++;
+    document.write('<p>' + contestantName + ' Guessed I have 2 brothers, which is correct.</p>');
   } else if (answer6 < 2) {
     alert('Sorry, that was wrong! You guessed too low!');
   } else if (answer6 > 2) {
@@ -177,7 +186,6 @@ for(var ind = 0; ind < 5; ind++){
   }
 }
 
-
 alert('You guessed ' + An7Array.length + ' correct! Their names were/are Jazz, Thumper, Fezzik, Arya and Maou.');
-
+document.write('<p>' + contestantName + ' was able to correctly guess the names of these pets: ' + An7Array + '</p>');
 console.log('There were',correct + ' correct');
